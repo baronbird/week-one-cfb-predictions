21fb1e2cafab1dc1b27a1143a28ca95e147833b7
Jacob Beiter <jbeiter@nd.edu>
2018-03-06 20:29:34 -0500

    scrape correct years (2015-2018)

--- scripts/scrape_schedule.py
+++ scripts/scrape_schedule.py
@@ -1,8 +1,8 @@
 import requests
 import re
 
-FIRST_YEAR = 2013
-LAST_YEAR = 2017
+FIRST_YEAR = 2015
+LAST_YEAR = 2018
 
 years = [str(year) for year in range(FIRST_YEAR,LAST_YEAR+1)]
 header = "year,away_team,home_team,neutral_site?,away_pts,home_pts"
@@ -24,13 +24,19 @@ with open("../data/schedule.csv", "w") as f:
             away_team, home_team = re.findall("<span>(.*?)</span>",game)[0:2]
             away_abbr, home_abbr = re.findall('<abbr title=".*?">(.*?)</abbr>',game)
             game_text = re.match('.*data-home-text="(at|vs)".*',game).group(1)
-            away_score = re.match(".*"+away_abbr+" (\d*).*",game).group(1)
-            home_score = re.match(".*"+home_abbr+" (\d*).*",game).group(1)
 
             if game_text == 'vs':
                 neutral_site = '1'
             else:
                 neutral_site = '0'
+
+            if year == "2018":
+                f.write(year+","+away_team+","+home_team+","+neutral_site+",N/A,N/A\n")
+                continue
+
+
+            away_score = re.match(".*"+away_abbr+" (\d*).*",game).group(1)
+            home_score = re.match(".*"+home_abbr+" (\d*).*",game).group(1)
 
             f.write(year+","+away_team+","+home_team+","+neutral_site+","+away_score+","+home_score+"\n")
         
