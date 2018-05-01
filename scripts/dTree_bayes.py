5cb8b8231d0709f4120ac0c4b8228c9f067473ca
Sam Alptekin <salpteki@nd.edu>
2018-04-30 22:59:05 -0400

    Graphs are good to go

--- scripts/dTree_bayes.py
+++ scripts/dTree_bayes.py
@@ -8,7 +8,7 @@ Created on Sun Apr 29 16:02:46 2018
 
 import matplotlib.pyplot as plt
 from graphviz import Source
-from sklearn import svm, metrics, grid_search, tree
+from sklearn import svm, metrics, grid_search, tree, naive_bayes
 from sklearn.tree import export_graphviz
 from sklearn.neighbors import KNeighborsClassifier
 import seaborn as sns
@@ -46,11 +46,12 @@ testing = testing.drop('away_team', 1).drop('home_team', 1).drop("Label", 1)
 
 iris = load_iris()
 
-
+'''
 machine = tree.DecisionTreeRegressor( criterion="mse" )
 machine.fit(training.as_matrix(), trainLabel.as_matrix())
 anew = metrics.accuracy_score(np.array(testLabel), machine.predict(testing.as_matrix()).round())
 print ("ID3      : ", anew)
+'''
 
 # Create a Decision Tree by iteratively adding the best features (independant of each other) to the model
 def feature_selection_best_ind(train_d, test_d):
@@ -71,31 +72,56 @@ def feature_selection_best_ind(train_d, test_d):
         # Add it to the priority queue
         heapq.heappush(pq, (-F1, feature))
     
+    
     # While adding a feature improves the F1 score, keep adding features
     old = 0
     new = .01
     features = []
-    while new > old:
+    x = []
+    y = []
+    #while new > old:
+    while pq:
         # Consider another feature
         features.append(heapq.heappop(pq)[1])
         train = train_d[features]
         test = test_d[features]
         
+        # Uncomment a machine
+        #machine = tree.DecisionTreeRegressor( criterion="mse" )
+        machine = naive_bayes.GaussianNB()
+        #machine = KNeighborsClassifier(p=len(training.columns.values)*2)
+        #machine = tree.DecisionTreeClassifier( criterion="entropy" )
+        
         # Determine the F1 score of the model
-        machine = tree.DecisionTreeRegressor( criterion="mse" )
         machine.fit(train.as_matrix(), trainLabel.as_matrix() )
         F1 = metrics.f1_score(np.array(testLabel), machine.predict(test.as_matrix()).round())
 
-        print(old, new, F1)
-
+        # Grab the F1 scores for graphing
+        x.append(len(features))
+        y.append(F1)
+        
         # Update condition variables
         old = new
         new = F1
     
+    # Graph the F1 scores as a function
+    plt.plot(x, y, '-')
+    plt.title('Incrementally Adding Best Independent Features')
+    plt.ylabel('F1 Score')
+    plt.xlabel('Number of Features')
+    plt.show()
+    
+    # As a bar chart
+    plt.bar(x, y)
+    plt.title('Incrementally Adding Best Independent Features')
+    plt.ylabel('F1 Score')
+    plt.xlabel('Number of Features')
+    plt.show()
+    
     features.pop()
     return features
 
-def feature_selection_best_dep(train_d, test_d, features, threshold):
+def feature_selection_best_dep(train_d, test_d, features, threshold, x, y):
     pq = []
 
     # Rank the features in order of F1 score when operating with current model
@@ -109,8 +135,13 @@ def feature_selection_best_dep(train_d, test_d, features, threshold):
         train = train_d[features]
         test = test_d[features]
         
+        # Uncomment a machine
+        #machine = tree.DecisionTreeRegressor( criterion="mse" )
+        machine = naive_bayes.GaussianNB()
+        #machine = KNeighborsClassifier(p=len(training.columns.values)*2)
+        #machine = tree.DecisionTreeClassifier( criterion="entropy" )
+        
         # Determine its F1 score
-        machine = tree.DecisionTreeRegressor( criterion="mse" )
         F1 = 0
         
         if len(features) == 1:
@@ -119,18 +150,19 @@ def feature_selection_best_dep(train_d, test_d, features, threshold):
         else:
             machine.fit(train.as_matrix(), trainLabel.as_matrix() )
             F1 = metrics.f1_score(np.array(testLabel), machine.predict(test.as_matrix()).round())
+
         
         # Throw that feature away
         features.pop()
         
         # If the model is an improvement, push the new feature onto the pq
-        if -F1 < threshold:    
-            heapq.heappush(pq, (-F1, feature))
+        #if -F1 < threshold:    
+        heapq.heappush(pq, (-F1, feature))
     
     # If pq is empty, adding additional features don't improve the model
     if not pq:
         return features
-    
+
     # Take the feature that gives the best improvement and build off of that
     chosen = heapq.heappop(pq)
     features.append(chosen[1])
@@ -138,8 +170,12 @@ def feature_selection_best_dep(train_d, test_d, features, threshold):
     
     print(chosen, threshold)
     
+    # Store number of features and F1 score
+    x.append(len(features))
+    y.append(-threshold)
+    
     # Recursive call to find another feature
-    return feature_selection_best_dep(train_d, test_d, features, threshold)
+    return feature_selection_best_dep(train_d, test_d, features, threshold, x, y)
     
     
 
@@ -147,17 +183,31 @@ def feature_selection_best_dep(train_d, test_d, features, threshold):
 def createDTree(training, testing):
     feature_selection_best_ind(training, testing)
     feature_selection_best_dep(training, testing, [], 0)
-    feature_selection_worst(training, testing, [], 0)
 
-#machine = KNeighborsClassifier(p=len(training.columns.values)*2)
-#machine = tree.DecisionTreeClassifier( criterion="entropy" )
 
 print(feature_selection_best_ind(training, testing))
 print("")
 
-print(feature_selection_best_dep(training, testing, [], 0))
+x = []
+y = []
+print(feature_selection_best_dep(training, testing, [], 0, x, y))
 print("")
 
+# Graph the F1 scores as a function
+plt.plot(x, y, '-')
+plt.title('Incrementally Adding Best Dependent Features')
+plt.ylabel('F1 Score')
+plt.xlabel('Number of Features')
+plt.show()
+
+# As a bar chart
+plt.bar(x, y)
+plt.title('Incrementally Adding Best Dependent Features')
+plt.ylabel('F1 Score')
+plt.xlabel('Number of Features')
+plt.show()
+
+'''
 # This is templates ignore it
 print("# This is templates ignore it")
 print(type(machine))
@@ -170,7 +220,7 @@ print("F1 Score:", metrics.f1_score( np.array(testLabel), machine.predict( testi
 print("Precision:", metrics.precision_score( np.array(testLabel), machine.predict( testing.as_matrix() ).round()))
 print("Recall:", metrics.recall_score( np.array(testLabel), machine.predict( testing.as_matrix() ).round()))
 
-'''
+
 graph = Source( tree.export_graphviz(machine, out_file=None, feature_names=training.columns))
 png_bytes = graph.pipe(format='png')
 with open('dtree_pipe_proposed_model.png','wb') as f:
